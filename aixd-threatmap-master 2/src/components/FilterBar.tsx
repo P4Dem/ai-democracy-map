54b2f7a3c7b708a4a32df3a010f432770446bcc2
nashthecoder <naijeria@gmail.com>
2026-06-12 21:35:27 +0300

    Show column header row always, not only when sticky

--- aixd-threatmap-master 2/src/components/FilterBar.tsx
+++ aixd-threatmap-master 2/src/components/FilterBar.tsx
@@ -184,27 +184,25 @@ export const FilterBar = ({
         </div>
       )}
 
-      {isSticky && (
-        <div className="-mx-4 mt-2.5 border-t border-border/30 px-4 pt-2">
-          <div className="flex items-center text-[11px] uppercase tracking-wider font-semibold text-muted-foreground">
-            <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.type }}>
-              Impact type
-            </div>
-            <div className="min-w-0 flex-1 px-2">
-              Description
-            </div>
-            <div className="min-w-0 flex-1 px-2">
-              Mitigation Strategy
-            </div>
-            <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.aspects }}>
-              Democracy Aspects
-            </div>
-            <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.source }}>
-              Source
-            </div>
+      <div className="-mx-4 mt-2.5 border-t border-border/30 px-4 pt-2">
+        <div className="flex items-center text-[11px] uppercase tracking-wider font-semibold text-muted-foreground">
+          <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.type }}>
+            Impact type
+          </div>
+          <div className="min-w-0 flex-1 px-2">
+            Description
+          </div>
+          <div className="min-w-0 flex-1 px-2">
+            Mitigation Strategy
+          </div>
+          <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.aspects }}>
+            Democracy Aspects
+          </div>
+          <div className="shrink-0 px-2" style={{ width: COL_WIDTHS.source }}>
+            Source
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
